72e24fafb22ea9ed9c4219dfda07525138983acc
Lokaler Chef <w15144@lehre.dhbw-stuttgart.de>
2016-10-07 11:24:03 +0200

    schnittstellenklasse angelegt, konstruktor noch nicht fertig, da nicht die richtige db abgerufen wird

---
+++ Aufwand/src/version1/schnittstelle.java
@@ -0,0 +1,41 @@
+package version1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class schnittstelle {
+
+	Connection con = null;
+	Statement stmt = null;
+
+	public schnittstelle() {
+		try // Treiber für mySQL laden
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Treiber gefunden\n--------------------------------");
+		} // try
+		catch (ClassNotFoundException e) {
+			System.out.println("Treiber NICHT gefunden\n--------------------------------");
+		} // catch
+
+		try // Connection zur Datenbank aufbauen
+		{
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/personal", "root", "");
+			System.out.println("Connection zur Datenbank aufgebaut\n--------------------------------");
+		} // try
+		catch (SQLException e) {
+			System.out.println("Connection zur Datenbank NICHT aufgebaut\n--------------------------------");
+		} // catch
+
+		try // Statement erzeugen
+		{
+			stmt = con.createStatement();
+			System.out.println("Statement erzeugt\n--------------------------------");
+		} // try
+		catch (SQLException e) {
+			System.out.println("Statement NICHT erzeugt\n--------------------------------");
+		} // catch
+	}
+}
