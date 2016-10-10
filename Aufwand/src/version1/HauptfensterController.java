7b4b4a3ca460065cc12317c24e48c8c81c1fc662
Jascha-Laptop <w15144@lehre.dhbw-stuttgart.de>
2016-10-10 11:25:31 +0200

    Weitere Funktionalitäten hinzugefügt. Projektfenster und Kompetenzenfenster sind grundsätzlich fertig. Jetzt fehtl nur noch die Phasenplanung.

--- Aufwand/src/version1/HauptfensterController.java
+++ Aufwand/src/version1/HauptfensterController.java
@@ -1,22 +1,16 @@
 package version1;
 
-import javafx.fxml.FXML;
-
-import javafx.scene.control.Button;
-
-import javafx.scene.control.TextField;
-
 import java.sql.SQLException;
-import java.util.List;
 
 import Tabellen.Projekt;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class HauptfensterController {
@@ -39,80 +33,82 @@ public class HauptfensterController {
 	@FXML
 	private TextField text_projektname;
 
-	
 	private ObservableList<Projekt> projektData;
 	private schnittstelle db;
-	
+
 	@FXML
 	private void initialize() throws SQLException {
 		System.out.println("Fenster startet");
-		
-    	db = new schnittstelle();
-    	
-    	projektData = FXCollections.observableArrayList(db.projekte_laden());
-    	
+
+		db = new schnittstelle();
+
+		projektData = FXCollections.observableArrayList(db.projekte_laden());
+
 		tblCell_projektID.setCellValueFactory(cellData -> cellData.getValue().getProjektidProperty().asObject());
 		tblCell_projektName.setCellValueFactory(cellData -> cellData.getValue().getProjektnameProperty());
-		
-		tbl_projektTabelle.setItems(projektData);    
-		
-		tbl_projektTabelle.getSelectionModel().selectedItemProperty().addListener(
-	            (observable, oldValue, newValue) -> tabelle_geklickt(tbl_projektTabelle.getSelectionModel().getSelectedItem()));
-	}
 
+		tbl_projektTabelle.setItems(projektData);
+
+		tbl_projektTabelle.getSelectionModel().selectedItemProperty().addListener((observable, oldValue,
+				newValue) -> tabelle_geklickt(tbl_projektTabelle.getSelectionModel().getSelectedItem()));
+	}
 
 	@FXML
 	public void tabelle_geklickt(Projekt p) {
-		
-		text_projektname.setText(p.getProjektname());						
-		
+
+		text_projektname.setText(p.getProjektname());
+
 	}
-	
+
 	@FXML
 	public void text_projektname_geklickt(ActionEvent event) throws SQLException {
-	
+
 		System.out.println("KlickText");
-		int anzahl = db.projektname_aendern(tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektid(), text_projektname.getText());
+		int anzahl = db.projektname_aendern(tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektid(),
+				text_projektname.getText());
 		System.out.println(anzahl + " Datensätze geändert!");
-		projektData.get(tbl_projektTabelle.getSelectionModel().getSelectedIndex()).setProjektname(text_projektname.getText());
+		projektData.get(tbl_projektTabelle.getSelectionModel().getSelectedIndex())
+				.setProjektname(text_projektname.getText());
 		tbl_projektTabelle.refresh();
-		
+
 	}
+
 	// Event Listener on Button[#button_projekt_anlegen].onAction
 	@FXML
 	public void button_projekt_anlegen_click(ActionEvent event) throws SQLException {
-		
 
 		int anzahl = db.projekt_anlegen();
 		System.out.println(anzahl + " Projekt(e) angelegt");
 		projektData = FXCollections.observableArrayList(db.projekte_laden());
-		tbl_projektTabelle.setItems(projektData);    			
-		
+		tbl_projektTabelle.setItems(projektData);
+
 	}
-	
+
+	@FXML
 	public void button_projekt_loeschen_click(ActionEvent event) throws SQLException {
-		
+
 		int anzahl = db.projekt_loeschen(tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektid());
 		System.out.println(anzahl + " Projekt(e) gelöscht");
 		projektData = FXCollections.observableArrayList(db.projekte_laden());
-		tbl_projektTabelle.setItems(projektData);	
-		
+		tbl_projektTabelle.setItems(projektData);
+
 	}
-	
+
 	public void button_kompetenzen_click(ActionEvent event) throws SQLException {
-		
-		if (tbl_projektTabelle.getSelectionModel().getSelectedItem() != null);{
-			
+
+		if (tbl_projektTabelle.getSelectionModel().getSelectedItem() != null)
+			;
+		{
+
 			try {
-				new OpenKompetenzStage(tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektid(), tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektname());
+				new OpenKompetenzStage(tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektid(),
+						tbl_projektTabelle.getSelectionModel().getSelectedItem().getProjektname());
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-		
+
 	}
-	
-	
+
 }
--- Aufwand/src/version1/schnittstelle.java
+++ Aufwand/src/version1/schnittstelle.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Tabellen.Kompetenz;
+import Tabellen.Mitarbeiterkompetenz;
 import Tabellen.Projekt;
-
-
+import Tabellen.Projektkompetenz;
 
 public class schnittstelle {
 
@@ -31,7 +31,8 @@ public class schnittstelle {
 
 		try // Connection zur Datenbank aufbauen
 		{
-			con = DriverManager.getConnection("jdbc:mysql://lolstats.org:3306/fallstudie_2016", "fallstudie_user2", "passwort123!");
+			con = DriverManager.getConnection("jdbc:mysql://lolstats.org:3306/fallstudie_2016", "fallstudie_user2",
+					"passwort123!");
 			System.out.println("Connection zur Datenbank aufgebaut\n--------------------------------");
 		} // try
 		catch (SQLException e) {
@@ -48,93 +49,151 @@ public class schnittstelle {
 		} // catch
 	}
 
-	public List<Projekt> projekte_laden() throws SQLException {	
-		
+	public List<Projekt> projekte_laden() throws SQLException {
+
 		List<Projekt> projekte = new ArrayList<Projekt>();
-		
+
 		try {
-			
+
 			rs = stmt.executeQuery("select * from Projekt");
-			
-			while (rs.next()){
-				projekte.add(new Projekt(rs.getInt("ProjektID"), rs.getString("Name")));				
+
+			while (rs.next()) {
+				projekte.add(new Projekt(rs.getInt("ProjektID"), rs.getString("Name")));
 			}
-			
-						
+
 		} catch (SQLException e) {
 			System.out.println("Fehler: " + e);
 		}
-		
-		
+
 		return projekte;
 
 	}
-	
-	
-	public List<Kompetenz> kompetenzen_laden() throws SQLException {	
-		
+
+	public List<Kompetenz> kompetenzen_laden() throws SQLException {
+
 		List<Kompetenz> kompetenzen = new ArrayList<Kompetenz>();
-		
+
 		try {
-			
+
 			rs = stmt.executeQuery("select * from Kompetenz");
-			
-			while (rs.next()){
-				kompetenzen.add(new Kompetenz(rs.getInt("KompetenzID"), rs.getString("Name")));				
+
+			while (rs.next()) {
+				kompetenzen.add(new Kompetenz(rs.getInt("KompetenzID"), rs.getString("Name")));
 			}
-			
-						
+
 		} catch (SQLException e) {
 			System.out.println("Fehler: " + e);
 		}
-		
-		
+
 		return kompetenzen;
 
 	}
-	
-	
+
 	public int projektname_aendern(int id, String neu) throws SQLException {
-		
+
 		int anzahl = 0;
 		try {
-			
+
 			anzahl = stmt.executeUpdate("update Projekt set Name = '" + neu + "' where ProjektID = " + id);
 
-						
 		} catch (SQLException e) {
 			System.out.println("Fehler: " + e);
 		}
 
-
 		return anzahl;
 	}
 
-	
-	
 	public int projekt_anlegen() throws SQLException {
-		
+
 		int anzahl = 0;
 		try {
-			
+
 			anzahl = stmt.executeUpdate("insert into Projekt (Name) values ('')");
-						
+
 		} catch (SQLException e) {
 			System.out.println("Fehler: " + e);
 		}
 		return anzahl;
 	}
-	
+
 	public int projekt_loeschen(int id) throws SQLException {
-		
+
 		int anzahl = 0;
 		try {
-			
+
 			anzahl = stmt.executeUpdate("delete from Projekt where ProjektID = " + id);
-						
+
 		} catch (SQLException e) {
 			System.out.println("Fehler: " + e);
 		}
 		return anzahl;
 	}
+
+	public List<Mitarbeiterkompetenz> mitarbeiterkompetenzen_laden(int kid) throws SQLException {
+
+		List<Mitarbeiterkompetenz> mitarbeiterkompetenzen = new ArrayList<Mitarbeiterkompetenz>();
+
+		try {
+			rs = stmt.executeQuery(
+					"select Mitarbeiter.MitarbeiterID, Mitarbeiter.Name, Mitarbeiter.Kosten_pro_PT, Mitarbeiter.MAK, Mitarbeiter.Zugehoerigkeit, Mitarbeiterkompetenz.MitarbeiterkompetenzID from Mitarbeiterkompetenz inner join Mitarbeiter on Mitarbeiterkompetenz.MitarbeiterID = Mitarbeiter.MitarbeiterID where Mitarbeiterkompetenz.KompetenzID = "
+							+ kid);
+			while (rs.next()) {
+				mitarbeiterkompetenzen.add(new Mitarbeiterkompetenz(rs.getInt("MitarbeiterID"), rs.getString("Name"),
+						rs.getInt("Kosten_pro_PT"), rs.getInt("MAK"), rs.getString("Zugehoerigkeit"),
+						rs.getInt("MitarbeiterkompetenzID")));
+			}
+		} catch (SQLException e) {
+			System.out.println("Fehler: " + e);
+		}
+		return mitarbeiterkompetenzen;
+
+	}
+
+	public List<Projektkompetenz> projektkompetenzen_laden(int pid) throws SQLException {
+
+		List<Projektkompetenz> projektkompetenzen = new ArrayList<Projektkompetenz>();
+
+		try {
+			rs = stmt.executeQuery(
+					"select Projektkompetenz.ProjektkompetenzID, Kompetenz.Name as kname, Mitarbeiter.Name as mname, Mitarbeiter.Kosten_pro_PT from Projektkompetenz inner join (Mitarbeiterkompetenz inner join Mitarbeiter on Mitarbeiter.MitarbeiterID = Mitarbeiterkompetenz.MitarbeiterID) on Projektkompetenz.MitarbeiterkompetenzID = Mitarbeiterkompetenz.MitarbeiterkompetenzID inner join Kompetenz on Kompetenz.KompetenzID = Projektkompetenz.KompetenzID where Projektkompetenz.ProjektID = "
+							+ pid);
+			while (rs.next()) {
+				projektkompetenzen.add(new Projektkompetenz(rs.getInt("ProjektkompetenzID"), rs.getString("kname"),
+						rs.getString("mname"), rs.getInt("Kosten_pro_PT")));
+			}
+		} catch (SQLException e) {
+			System.out.println("Fehler: " + e);
+		}
+		return projektkompetenzen;
+
+	}
+
+	public int projektkompetenz_anlegen(int mkid, int pid, int kid) throws SQLException {
+
+		int anzahl = 0;
+		try {
+
+			anzahl = stmt.executeUpdate(
+					"insert into Projektkompetenz (MitarbeiterkompetenzID, ProjektID, KompetenzID) values (" + mkid
+							+ ", " + pid + ", " + kid + ")");
+
+		} catch (SQLException e) {
+			System.out.println("Fehler: " + e);
+		}
+		return anzahl;
+	}
+
+	public int projektkompetenz_loeschen(int pkid) throws SQLException {
+
+		int anzahl = 0;
+		try {
+
+			anzahl = stmt.executeUpdate("delete from Projektkompetenz where ProjektkompetenzID = " + pkid);
+
+		} catch (SQLException e) {
+			System.out.println("Fehler: " + e);
+		}
+		return anzahl;
+	}
+
 }
